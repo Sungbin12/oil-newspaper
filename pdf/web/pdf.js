156a14459e865ec51d6d8cc5150c50c69fcc5fab
Sungbin12 <123452136+Sungbin12@users.noreply.github.com>
2025-02-01 13:19:27 +0900

    pdf.js 업데이트

--- pdf/web/pdf.js
+++ pdf/web/pdf.js
@@ -24,8 +24,5 @@ window.onload = function() {
         // 3. 네이버 브라우저 확인
     var userAgent = navigator.userAgent;
     if (userAgent.indexOf("NAVER") > -1) {
-        alert("네이버 브라우저로 접속할 경우에는 정상적인 서비스가 불가능합니다.\n다른 브라우저로 다시 접속해주세요.");
-        
-        // 현재 페이지 기록을 삭제하고 바로 리디렉션
-        window.location.replace("../../../../../../../"); // 원하는 페이지 URL로 변경
+        alert("네이버 브라우저로 접속할 경우에는 정상적인 서비스가 불가능합니다.\n다른 브라우저로 다시 접속해주세요."); window.location.replace("../../../../../../../"); // 원하는 페이지 URL로 변경
     }
