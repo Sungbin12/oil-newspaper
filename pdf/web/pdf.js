3f9d8649f91bd4205efc1edd53cb4d03c12a4a3a
Sungbin12 <123452136+Sungbin12@users.noreply.github.com>
2025-02-02 09:59:36 +0900

    update 3

--- pdf/web/pdf.js
+++ pdf/web/pdf.js
@@ -19,17 +19,18 @@ window.onload = function() {
         html: 'PDF 로딩 문제로 PDF Viewer이 변경되었으니, 서비스 이용에 불편 없으시길 바랍니다.',
         icon: 'info',
         confirmButtonText: '확인'
+    }).then(() => {
+        // SweetAlert2의 확인 버튼을 클릭한 후에 진행할 동작
+        // 여기서 쿠키 확인 또는 다른 처리를 할 수도 있음.
     });
 
-     // 3. 네이버 차단
-    window.onload = function() {
-            // 네이버 브라우저의 User-Agent 검사
-            var userAgent = navigator.userAgent.toLowerCase();
-            
-            // 네이버 브라우저에 해당하는 User-Agent 문자열 확인
-            if (userAgent.indexOf("naver") !== -1) {
-                alert("네이버 브라우저로 접속할 경우에는 정상적인 서비스가 불가능합니다.\n다른 브라우저로 다시 접속해주세요.");
-                // 접근 차단 후 다른 페이지로 이동
-                window.location.replace("../../../../../../../"); // 원하는 페이지로 리디렉션
-            }
-        };
+    // 3. 네이버 차단
+    var userAgent = navigator.userAgent.toLowerCase();
+    
+    // 네이버 브라우저에 해당하는 User-Agent 문자열 확인
+    if (userAgent.indexOf("naver") !== -1) {
+        alert("네이버 브라우저로 접속할 경우에는 정상적인 서비스가 불가능합니다.\n다른 브라우저로 다시 접속해주세요.");
+        // 접근 차단 후 다른 페이지로 이동
+        window.location.replace("../../../../../../../"); // 원하는 페이지로 리디렉션
+    }
+};
